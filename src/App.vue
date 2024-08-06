9068e79f15469467dfb2d347118720f100c0cd4d
Riran Uishi <riran.uishi@gmail.com>
2024-08-06 20:18:42 +0900

    component test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
   <div>
-    <p>
-      サンプル
+    <myheader></myheader>
+    <p v-if="msg.length > 0">
+      {{msg}}
     </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
   </div>
 </template>
+
+<script>
+import myheader from './components/my-header'
+
+export default {
+  components: {
+    myheader
+  },
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  }
+}
+</script>
---
+++ src/views/Blogs.vue
@@ -0,0 +1,6 @@
+<template>
+    <div id="blogs_container">
+      ブログページ
+    </div>
+  </template>
+  
